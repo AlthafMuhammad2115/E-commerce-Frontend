ea65bd33613e01aade2b2330d226eb00471f072e
Muhammad Althaf P A <128133867+AlthafMuhammad2115@users.noreply.github.com>
2024-03-17 21:02:59 +0530

    Update home.component.ts

--- src/app/contentpage/home/home.component.ts
+++ src/app/contentpage/home/home.component.ts
@@ -10,12 +10,51 @@ import { HomeserviceService } from 'src/app/service/homeservice.service';
 })
 export class HomeComponent {
   state:any;
-  men: { img: string; name: string; }[];
-  person: { img: string; name: string; routerLink:string }[];
   constructor(private home :HomeserviceService){
     this.state=false;
-    this.men=this.home.Men
-   this.person=this.home.person
   }
+  public men=[
+    {
+      img:"../../../assets/jeans.png",
+      name:"Jeans"
+    },
+    {
+      img:"../../../assets/hoodie.png",
+      name:"Hoodies"
+    },
+    {
+      img:"../../../assets/t-shirt.png",
+      name:"T-shirts"
+    },
+    {
+      img:"../../../assets/shirt.png",
+      name:"Shirts"
+    },
+    {
+      img:"../../../assets/trouser.png",
+      name:"Trousers"
+    },
+    {
+      img:"../../../assets/cottonpant.png",
+      name:"Pants"
+    },
+  ]
+  public person=[
+    {
+      img:"../../../assets/men.png",
+      name:"Men",
+      routerLink:"/Men"
+    },
+    {
+      img:"../../../assets/women.png",
+      name:"Women",
+      routerLink:"/Women"
+    },
+    {
+      img:"../../../assets/kids.png",
+      name:"Kids",
+      routerLink:"/Kids"
+    }
+  ]
   
 }
