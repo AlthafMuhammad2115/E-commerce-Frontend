f255fe5aacaaa73b00ca05a7f22e42d713e3f9d7
Muhammad Althaf P A <128133867+AlthafMuhammad2115@users.noreply.github.com>
2024-03-17 21:01:25 +0530

    Update homeservice.service.ts

--- src/app/service/homeservice.service.ts
+++ src/app/service/homeservice.service.ts
@@ -6,47 +6,5 @@ import { Injectable } from '@angular/core';
 export class HomeserviceService {
 
   constructor() { }
-  public Men=[
-    {
-      img:"../../../assets/jeans.png",
-      name:"Jeans"
-    },
-    {
-      img:"../../../assets/hoodie.png",
-      name:"Hoodies"
-    },
-    {
-      img:"../../../assets/t-shirt.png",
-      name:"T-shirts"
-    },
-    {
-      img:"../../../assets/shirt.png",
-      name:"Shirts"
-    },
-    {
-      img:"../../../assets/trouser.png",
-      name:"Trousers"
-    },
-    {
-      img:"../../../assets/cottonpant.png",
-      name:"Pants"
-    },
-  ]
-  public person=[
-    {
-      img:"../../../assets/men.png",
-      name:"Men",
-      routerLink:"/Men"
-    },
-    {
-      img:"../../../assets/women.png",
-      name:"Women",
-      routerLink:"/Women"
-    },
-    {
-      img:"../../../assets/kids.png",
-      name:"Kids",
-      routerLink:"/Kids"
-    }
-  ]
+  
 }
